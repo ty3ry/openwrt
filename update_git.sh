2df60adafb2725ee5c2fb4e9f87d42da8dedec43
ty3ry <cosmas.es08@gmail.com>
2020-11-05 14:33:33 +0700

    - initial clone

---
+++ update_git.sh
@@ -0,0 +1,10 @@
+USER_MAIL="cosmas.es08@gmail.com"
+USER_NAME="ty3ry"
+
+echo "Set git account"
+echo "User email : $USER_MAIL"
+echo "User name : $USER_NAME"
+
+git config user.email $USER_MAIL
+git config user.name $USER_NAME
+
